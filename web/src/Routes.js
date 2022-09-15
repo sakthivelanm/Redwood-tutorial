8160d5dab1c7627cb36ecd21dd9f102cfa815ef1
sakthivelanm <sakthivelanm@gmail.com>
2022-09-15 14:55:19 -0400

    Chapter 1

---
+++ web/src/Routes.js
@@ -0,0 +1,16 @@
+import { Router, Route, Set } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const Routes = () => {
+  return (
+    <Router>
+      <Set wrap={BlogLayout}>
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
+      <Route notfound page={NotFoundPage} />
+    </Router>
+  )
+}
+
+export default Routes
